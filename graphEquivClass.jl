0073a64bd3f86fccb11db1a3d422b307dd4d51c0
Roni <roniapollo1@gmail.com>
2024-07-21 14:56:35 +0300

    added names of functions and documentation of what they do for organisation when coding

---
+++ graphEquivClass.jl
@@ -0,0 +1,19 @@
+#Equivalency is by isomorphism in this case. Equivalence class refers to all
+#graphs isomorphic to each other
+
+# function that returns table of equivalence class of graph produced by change
+# of every one edge of each graph equivalence class 
+function changesTable()
+    
+end
+
+# helper function that returns dictionary with keys being graphs
+# and values being graph equivalency class 
+function equivClassDict()
+
+end
+
+# helper function that adds all permutations of given graph to given dictionary
+function addPermutations(graph, equivClass, dictionary)
+
+end
